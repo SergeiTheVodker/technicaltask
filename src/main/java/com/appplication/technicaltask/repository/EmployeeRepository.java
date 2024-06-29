680c58fc9ab5b6692b68ed6e21ed7775614cc69c
JC Suarez <sargedines@gmail.com>
2024-06-29 14:51:34 +0800

    Implement JPA repository
    Add JPA repository for the Employee entity.

--- src/main/java/com/appplication/technicaltask/repository/EmployeeRepository.java
+++ src/main/java/com/appplication/technicaltask/repository/EmployeeRepository.java
@@ -1,5 +1,9 @@
 package com.appplication.technicaltask.repository;
 
-public interface EmployeeRepository {
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
 
-}
+import com.appplication.technicaltask.model.Employee;
+
+@Repository
+public interface EmployeeRepository extends JpaRepository<Employee, Long>{ }
